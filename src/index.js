9c4d2d8d9428d3d5575accb667cbc2d3f05d530b
RenanRambul <renanrambuls@gmail.com>
2021-05-27 07:53:42 -0300

    Counter Context

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/global-styles.css';
+import { CounterContextProvider } from './contexts/CounterContext';
 
 import { Home } from './templates/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <CounterContextProvider>
+      <Home />
+    </CounterContextProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,4 +1,6 @@
 export const Home = () => {
+  const [state, dispatch] = useConterContext();
+
   return (
     <div>
       <h1>Oi</h1>
